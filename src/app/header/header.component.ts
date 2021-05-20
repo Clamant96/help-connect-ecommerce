0f193a1eb0208845d38c52210a5fb2c345110647
Clamant96 <kevin.lazzarotto@hotmail.com>
2021-05-20 11:40:18 -0300

    Atualizacao layout components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment.prod';
+import { Produto } from '../model/Produto';
 import { AuthService } from '../service/auth.service';
+import { PedidoService } from '../service/pedido.service';
 
 @Component({
   selector: 'app-header',
@@ -13,12 +15,28 @@ export class HeaderComponent implements OnInit {
   usuario = environment.usuario;
   nome = environment.nome;
 
+  listaDeProdutos: Produto[];
+  tamanhoCarrinho: number;
+
   constructor(
-    public auth: AuthService
+    public auth: AuthService,
+    private pedidoService: PedidoService
 
   ) { }
 
   ngOnInit() {
+
+    this.findByIdProdutosCarrinho();
+
+  }
+
+  findByIdProdutosCarrinho() {
+    this.pedidoService.findAllByProdutosPedidos(environment.pedidos).subscribe((resp: Produto[]) => {
+      this.listaDeProdutos = resp;
+
+      this.tamanhoCarrinho = this.listaDeProdutos.length;
+
+    })
 
   }
 
